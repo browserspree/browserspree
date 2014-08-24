9d8dd58cc55c003b7cbd26b8b6fe18adf8d4e5a4
Britney Wright <blw06g@gmail.com>
2014-08-24 17:18:21 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  mount Spree::Core::Engine, :at => "/shop"
+  mount Spree::Core::Engine, :at => "/"
 
   mount_browsercms
 end
